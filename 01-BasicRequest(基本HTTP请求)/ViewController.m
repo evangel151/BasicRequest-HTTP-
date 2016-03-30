a3b563011a26a8aed59afb3f707c3804d83cb11b
Asuka <154906186@qq.com>
2016-03-30 20:36:44 +0800

    BasicRequest
    在本机搭建JaveEE服务器后。
    最基本的网络请求，可以获得服务器返回的数据 ，但无法显示网络返回的图形界面。

--- 01-BasicRequest(基本HTTP请求)/ViewController.m
+++ 01-BasicRequest(基本HTTP请求)/ViewController.m
@@ -7,8 +7,13 @@
 //
 
 #import "ViewController.h"
+#import "MBProgressHUD+MJ.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *userName;
+@property (weak, nonatomic) IBOutlet UITextField *pwd;
+
+- (IBAction)login;
 
 @end
 
@@ -16,12 +21,50 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// 点击屏幕其他地方的时候退出编辑
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+    
+    [self.view endEditing:YES];
 }
 
+- (IBAction)login {
+    // 1. 用户名
+    NSString *userNameText = self.userName.text;
+    if (userNameText.length == 0) {
+        [MBProgressHUD showError:@"请输入用户名"];
+        return;
+    }
+    
+    // 2. 密码
+    NSString *pwdText = self.pwd.text;
+    if (pwdText.length == 0) {
+        [MBProgressHUD showError:@"请输入密码"];
+        return;
+    }
+    
+    /**
+     接口文档: 定义描述服务器端的请求接口
+     1> 请求路径URL: 客户端应该请求哪个路径
+     http://localhost:8080/MJServer/login 
+     
+     2> 请求参数: 客户端要发给服务器的数据
+     username - 用户名
+     pwd - 密码
+     3> 请求结果: 服务器会返回什么东西给客户端
+     */
+    
+    // 如果用户名和密码都不为0 -> 发送用户名和密码到服务器 (使用HTTP协议)
+    // 创建一个URL
+    NSString *urlStr = [NSString stringWithFormat:@"http://localhost:8080/MJServer/login?username=%@&pwd=%@", userNameText, pwdText];
+    NSURL *url = [NSURL URLWithString:urlStr];
+    // 创建一个请求
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    // 发送一个同步请求 (在主线程发送请求)  NSURLConnection 在iOS9中已经不推荐使用
+    NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    
+    NSLog(@"%@", data); // 服务器正在工作的话，返回本地服务器数据  (因为转码问题，无法显示汉字)
+}
 @end
